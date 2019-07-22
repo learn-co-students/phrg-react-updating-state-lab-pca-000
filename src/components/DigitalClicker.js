f37f926f3b5b45dbca124ad9f939ad8ee2e6f017
Ezz Abousaif <ezzeldin.abousaif@powerhrg.com>
2019-07-22 10:20:36 -0400

    updating state lab

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  increment = () => {
+    let num = this.state.timesClicked + 1
+    this.setState({
+      timesClicked: num
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.increment}>{this.state.timesClicked}</button>
+      </div>
+    );
+  }
+}
